fbd2f5e989e52de26e4fa8c547c6e834a9b3b004
Rachel Michel <michel.rachel@hotmail.fr>
2020-09-23 08:54:10 +0200

    First commit

--- README.md
+++ README.md
@@ -16,3 +16,5 @@ Lancer un `npm install`
 **Watch** : npm run dev
 
 **Build** : npm run prod
+
+**Attention ! Dans le .postcssrc si modules est à true le nom des classes CSS sera modifié et donc plus viable avec le css en place !**
--- index.js
+++ index.js
@@ -3,7 +3,39 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import 'reset-css'
 import './src/css/main.scss'
 
-// Custom
-import {test} from './src/js/test'
+// JS
+import $ from 'jquery'
 
-document.querySelector('h1').textContent = test("Comment ça vas l'ami ?");
+$(document).ready(() => {
+
+    window.matchMedia("(min-width: 800px)").addListener(() => {
+        if ($(window).width() >= 800) {
+            $('.nav-slide').css('display', 'flex');
+        } else {
+            $('.nav-slide').css('display', 'none');
+        }
+    });
+
+    $('.nav__pseudo').click(() => {
+        let $navList = $('.nav-list');
+
+        if ($navList.hasClass('nav-list_hide')) {
+            $navList.removeClass('nav-list_hide');
+            $navList.addClass('nav-list_show');
+        } else {
+            $navList.removeClass('nav-list_show');
+            $navList.addClass('nav-list_hide');
+        }
+    });
+
+    $('.nav__hamburger').click(() => {
+        let $navSlide = $('.nav-slide');
+
+        if ($navSlide.is(':visible')) {
+            $navSlide.slideUp()
+        } else {
+            $navSlide.slideDown()
+        }
+    });
+
+});
